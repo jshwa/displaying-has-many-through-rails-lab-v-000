e4a73abe31a90839259a5d327999e33ef58bb3c6
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-15 21:40:10 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def human_time
-    self.appointment_datetime.strftime("%B %d, %Y at %i:%M")
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
   end
 end
